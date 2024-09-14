daa5c6673ee6be177d4874ae495cef3cb8120133
Branko Pualić-Radujko <pualicbranko@gmail.com>
2024-09-14 23:40:47 +0200

    [FEAT] - Post - Default image if null

---
+++ Common/ImageUrlToDefaultConverter.cs
@@ -0,0 +1,15 @@
+﻿namespace InkWell.MAUI.Common;
+
+public class ImageUrlToDefaultConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+	{
+		var imageUrl = value as string;
+		return string.IsNullOrEmpty(imageUrl) ? "dotnet_bot.png" : imageUrl;
+	}
+
+	public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
